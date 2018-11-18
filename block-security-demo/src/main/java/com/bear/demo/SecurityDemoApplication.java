422f94789975eb108307462d667b11eb02cb41b0
ybeario <571523412@qq.com>
2018-11-18 13:40:33 +0800

    多模块项目结构搭建完毕

---
+++ block-security-demo/src/main/java/com/bear/demo/SecurityDemoApplication.java
@@ -0,0 +1,17 @@
+package com.bear.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Y.bear
+ * @version 创建时间：2018年11月17日 下午7:11:06 类说明
+ */
+@SpringBootApplication
+public class SecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDemoApplication.class, args);
+	}
+
+}
